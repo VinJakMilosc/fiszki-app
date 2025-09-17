6ba79b28a7ffeb60b82025bb1ea162065ad257ad
VinJakMilosc <miko.prywatnie@gmail.com>
2025-09-17 13:21:29 +0200

    dodanie kart

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Flashcard from './FlashCard'
 
 function App() {
   
   return (
-      <div></div>   
+    <>
+    <Flashcard question="Ile cm jest w 14cm?" answer="14cm"/>
+    <Flashcard question="Iloczyn liczb 11 * 11?" answer="121"/>
+    <Flashcard question="ile Asia zjada jablek dnia 10.07?" answer="nie zjada nic, umara tydzień przed"/>
+    <Flashcard question="ile karatów ma zte laboubou?" answer="24karaty"/>
+    </>
   )
 }
 
 export default App
+ 
