c910dca61594d5808749b69ddbc18298d26dc59b
VinJakMilosc <miko.prywatnie@gmail.com>
2025-09-17 13:25:12 +0200

    update karty

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Flashcard question="Ile cm jest w 14cm?" answer="14cm"/>
     <Flashcard question="Iloczyn liczb 11 * 11?" answer="121"/>
     <Flashcard question="ile Asia zjada jablek dnia 10.07?" answer="nie zjada nic, umara tydzień przed"/>
-    <Flashcard question="ile karatów ma zte laboubou?" answer="24karaty"/>
+    <Flashcard question="ile karatów ma zlote laboubou?" answer="24 karaty"/>
     </>
   )
 }
